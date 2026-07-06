ee49e4f3d2335544bdda5b8daa1b7e4fc0efa6cb
Zahra Ahmadi <zahra.ahmedi2020@gmail.com>
2026-07-06 10:17:42 +0330

    and hide and show listfeatuer

--- my-profile/script.js
+++ my-profile/script.js
@@ -1,2 +1,5 @@
 function showMessage() { alert("Thank you for visiting."); }
 function changebackground() { document.body.style.backgroundColor="lightgreen"; }
+function changeTitle() {let heading = document.getElementById("1");heading.innerText='Welcome! Zahra'; }
+function HideList() { document.getElementById("2").style.display="none";}
+function showList() { document.getElementById("2").style.display="block";}
