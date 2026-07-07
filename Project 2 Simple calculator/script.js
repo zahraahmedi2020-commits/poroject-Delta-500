d37b54fb9972b8d17bca68d4c49ce9e128cba076
Zahra Ahmadi <zahra.ahmedi2020@gmail.com>
2026-07-07 17:17:28 +0330

    Refacutor calculator using swich

--- Project 2 Simple calculator/script.js
+++ Project 2 Simple calculator/script.js
@@ -1,6 +1,25 @@
 
-function add(){ let heading = document.getElementById("1");
-                     let box1=document.getElementById("one").value;
+function Calculator(operator){ let heading= document.getElementById("R");
+                      let box1=document.getElementById("one").value;
                       let box2=document.getElementById("tow").value;
-                      let sum= Number(box1) + Number(box2);
-                         heading.innerText= sum;}
+                      let sum;
+                  switch (operator){
+                      case "+":
+                                sum= Number(box1) + Number(box2);
+                                break;
+                      case "-":
+                                sum= Number(box1) - Number(box2);
+                                break;
+                      case "*":
+                               sum= Number(box1) * Number(box2);
+                               break;
+                      case "/":
+                               sum= Number(box1) / Number(box2);
+                               break;
+                     default:
+                              alert('invalid operator');
+ }
+
+                heading.innerText= sum;
+
+ }
