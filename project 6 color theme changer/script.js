68fb0ca6013db74c2d5d4fbcb124a0bcfd8534e9
Zahra Ahmadi <zahra.ahmedi2020@gmail.com>
2026-07-08 19:01:09 +0330

    complete project 6-color theme changer

---
+++ project 6 color theme changer/script.js
@@ -0,0 +1,18 @@
+function changebackground(color) {
+switch (color){
+                      case "blue":
+                               document.body.style.backgroundColor="blue";
+                                break;
+                      case "green":
+                                document.body.style.backgroundColor="green";
+                                break;
+                      case "yellow":
+                              document.body.style.backgroundColor="yellow";
+                               break;
+                      case "White":
+                               document.body.style.backgroundColor="white";
+                               break;
+                     default:
+                             document.body.style.backgroundColor="black";
+ }
+ }
