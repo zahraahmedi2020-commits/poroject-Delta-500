1866832593d8333d07f19023ee8b92ff50f548d1
Zahra Ahmadi <zahra.ahmedi2020@gmail.com>
2026-07-23 14:46:43 +0330

    Done complete  project 13:student menagment Pro

--- project13  student Manager Pro/script.js
+++ project13  student Manager Pro/script.js
@@ -1,21 +1,21 @@
 var currentIndex = 0
 let score=0;
 let students=[];
+let editindex=-1;
 function mainmenu(){
 document.getElementById("body").style.display="block";
 document.getElementById("dshow").style.display="none";
+document.getElementById("dadd").style.display="none";
 document.getElementById("ddelet").style.display="none";
-document.getElementById("dshow").style.display="none";
 document.getElementById("dsearch").style.display="none";
 document.getElementById("daverage").style.display="none";
 document.getElementById("dchangesc").style.display="none";
-document.getElementById("dmaxscore").style.display="none";
-document.getElementById("dminscore").style.display="none";
+document.getElementById("dmax").style.display="none";
+document.getElementById("dmin").style.display="none";
 document.getElementById("dsorte").style.display="none";
 document.getElementById("rsearch").innerHTML="";
 document.getElementById("result").innerHTML="";
 document.getElementById("rscore").innerHTML="";
-document.getElementById("sortesc").innerHTML="";
 document.getElementById("sids").value="";
 document.getElementById("sns").value="";
 document.getElementById("dids").value="";
@@ -29,16 +29,27 @@ function saves(){
      let ag=document.getElementById("inputag");
      let sc=document.getElementById("inputsc");
      let m=document.getElementById("inputma");
-     let student={fullname:n.value,ids:id,age:ag.value,score:sc.value,major:m.value};
+     if(n.value.trim()==""){
+        alert("name is required");
+        return;
+     }else{
+        if(sc.value<0||sc.value>20){
+            alert("score must be between 0 and 20");
+            return;
+        }else{
+              let student={fullname:n.value,ids:id,age:ag.value,score:sc.value,major:m.value};
      students.push(student);
      currentIndex++;
      alert("student save successfully");
      n.value="";
      m.value="";
-     ag.value="";
-     sc.value="";
+     ag.value=0;
+     sc.value=0;
     document.getElementById("dadd").style.display="none";
-     document.getElementById("body").style.display="block";}
+     document.getElementById("body").style.display="block";
+        }
+     }
+     }
 function shows(){
       document.getElementById("body").style.display="none";
       document.getElementById("dshow").style.display="block";   
@@ -51,6 +62,9 @@ function shows(){
           for(let i=0; i<students.length; i++){ 
                let r = document.getElementById("result");
                let d =document.createElement("div");
+               if(students[i].score >=17){d.style.color="green";}
+               else if(students[i].score>=10){d.style.color="orange";}
+               else {d.style.color="red";}
               d.innerHTML ="fullname:" + students[i].fullname + ", id student:" + students[i].ids + ", age:" + students[i].age + ", score:" + students[i].score + ", Major:"+students[i].major + "<br>"; 
                r.appendChild(d);}
          }
@@ -111,17 +125,71 @@ function average(){
      let r=document.getElementById("hsc");
      r.innerText=avg;
      }
-function edit(){
-    document.getElementById("body").style.display="none";
-    document.getElementById("dadd").style.display="block";
-    document.getElementById("save").style.display="none";
-    document.getElementById("update").style.display="block";
-    document.getElementById("headingform").innerText="Edit Student";
+     
+
+function findedit(){
+     document.getElementById("body").style.display="none";
+     document.getElementById("ddelet").style.display="block";
+     document.getElementById("headingd").innerHTML="edit Student whith Id ";
+     let bs=document.getElementById("bdelet");
+     bs.innerText="search";
+     bs.onclick=edit;
 }
-function findedit(){}
+function edit(){
+   document.getElementById("body").style.display="none";
+   document.getElementById("ddelet").style.display="none";
+   let ninput=document.getElementById("inputn");
+   let ainput=document.getElementById("inputag");
+   let scinput=document.getElementById("inputsc");
+   let mainput=document.getElementById("inputma");
+   let ide=Number(document.getElementById("dids").value); 
+    if(students.length==0){
+         alert("no student ");
+         document.getElementById("dchangesc").style.display="none";
+         document.getElementById("body").style.display="block"; 
+       }else{ let found=false;
+       for(let i=0; i<students.length; i++){ 
+         if(students[i].ids==ide){
+             document.getElementById("dadd").style.display="block";
+             document.getElementById("save").style.display="none";
+             document.getElementById("update").style.display="block";
+             document.getElementById("headingform").innerText="Edit Student";
+             editindex=i; 
+              ninput.value=students[editindex].fullname;
+              ainput.value= students[editindex].age;
+              scinput.value= students[editindex].score;
+              mainput.value= students[editindex].major;
+              found=true;}}
+              
+            }
+}
+  function updates(editindex){
+     let ninput=document.getElementById("inputn");
+     let ainput=document.getElementById("inputag");
+     let scinput=document.getElementById("inputsc");
+     let mainput=document.getElementById("inputma");
+   if(students.length==0){
+         alert("no student ");
+         document.getElementById("dchangesc").style.display="none";
+         document.getElementById("body").style.display="block"; 
+       }else{ let found=false;     
+             students[editindex].fullname= ninput.value;
+             students[editindex].age=ainput.value;
+             students[editindex].score=scinput.value;
+             students[editindex].major=mainput.value;
+              found=true;
+               alert("susseccfully update student");
+            document.getElementById("dadd").style.display="none";
+
+              shows();
+            }
+  
+  }            
+
 function changesc(){
 document.getElementById("body").style.display="none";
 document.getElementById("dchangesc").style.display="block";
+document.getElementById("updatesc").style.display="none";
 }
 function findsc(){
     document.getElementById("chscs").style.display="block";
@@ -149,21 +217,78 @@ function findsc(){
 }
 function maxsc(){
     document.getElementById("body").style.display="none";
-    document.getElementById("dmaxscore").style.display="block";
-    let max=0;
+    document.getElementById("dmax").style.display="block";
+      let m=Number(students[0].score);
+    if (students.length === 0) {
+    alert("not  anything ");
+    return;
+}else{
     for(let i=0;i<students.length;i++){
-        max=max(Number(students[i].score));}
-     let r=document.getElementById("hmaxsc");
-     r.innerText=max;
+        if(Number(students[i].score) > m ){
+        m=Number(students[i].score);}
+    }
+    let r=document.getElementById("hmaxsc");
+     r.innerText=m;
+}
      }
      function minsc(){
       document.getElementById("body").style.display="none";
-      document.getElementById("dminscore").style.display="block";
-  
-     }
+      document.getElementById("dmin").style.display="block";
+    let r=document.getElementById("hminsc");
+      let m=Number(students[0].score);
+       if (students.length === 0) {
+    alert("not  anything ");
+    return;
+}else{
+    for(let i=0;i<students.length;i++){
+        if(Number(students[i].score) < m ){
+        m=Number(students[i].score);}
+    }
+    
+     r.innerText=m;
+     } 
+    }
   function sortesc(){
      document.getElementById("body").style.display="none";
      document.getElementById("dsorte").style.display="block";
   }
-
+  function Asec(){
+ document.getElementById("body").style.display="none";
+     document.getElementById("dsorte").style.display="block";
+     let r=document.getElementById("sortesc");
+     let m=Number(students[0].score);
+    if (students.length === 0) {
+    alert("not  anything ");
+    return;
+}else{
+    for(let i=0;i<students.length;i++){
+        for(let j=i+1;j<students.length;j++){
+        if( Number(students[j].score) > Number(students[i].score) ){
+        let temp=students[i];
+        students[i]=students[j];
+        students[j]=temp;}
+    }
+    document.getElementById("dsorte").style.display="none";
+}shows();
+}}
+function Desc(){
+document.getElementById("body").style.display="none";
+     document.getElementById("dsorte").style.display="block";
+     let r=document.getElementById("sortesc");
+     let m=Number(students[0].score);
+    if (students.length === 0) {
+    alert("not  anything ");
+    return;
+}else{
+    for(let i=0;i<students.length;i++){
+        for(let j=i+1;j<students.length;j++){
+        if( Number(students[j].score) < Number(students[i].score) ){
+        let temp=students[i];
+        students[i]=students[j];
+        students[j]=temp;}
+    }
+    document.getElementById("dsorte").style.display="none";
+}shows();
+} 
+}
   
