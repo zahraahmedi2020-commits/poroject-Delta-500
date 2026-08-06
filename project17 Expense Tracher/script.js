b77b1fcf6441535f12888749170edf8605eff6a2
Zahra Ahmadi <zahra.ahmedi2020@gmail.com>
2026-08-06 17:10:56 +0330

    Done complete edit transaction project 17

--- project17 Expense Tracher/script.js
+++ project17 Expense Tracher/script.js
@@ -53,3 +53,51 @@ function savet(){
        console.log(Transaction);}
        }
 }
+
+function editt(){
+  document.getElementById("dedit").style.display="block";
+  document.getElementById("del").style.display="none";
+  document.getElementById("dela").style.display="none";
+
+}
+function findeditt(){
+  let ide=Number(document.getElementById("eidt").value); 
+  let tinput=document.getElementById("inputt");
+  let aminput=document.getElementById("inputam");
+  let typeinput=document.getElementById("type");
+  empty();
+  if(aminput.value<0){
+          alert("Amount Not Could Negetive");
+         return;
+     }else{
+  let found=false;
+  for(let i=0; i<Trackers.length; i++){ 
+    if(Trackers[i].IdT==ide){
+      document.getElementById("dedit").style.display="none";
+      document.getElementById("dadd").style.display="block";
+      document.getElementById("save").style.display="none";
+      document.getElementById("update").style.display="block";
+      editindex=i;
+      
+      tinput.value= Trackers[editindex].title;
+      aminput.value= Trackers[editindex].amount;
+      typeinput.value=Trackers[editindex].type;
+      Trackers[i].Date=getcurrentDate();
+      console.log(Trackers[i]);
+      found=true;}
+    }if(!found)alert("not found Transaction whtih this ID .");
+     }
+}
+function updateedit(editindex){
+ let tinput=document.getElementById("inputt");
+ let aminput=document.getElementById("inputam");
+ let typeinput=document.getElementById("type");
+ Trackers[editindex].title=tinput.value;
+ Trackers[editindex].amount=aminput.value;
+ Trackers[editindex].type=typeinput.value;
+ localStorage.setItem("Trackers",JSON.stringify(Trackers));
+ alert("susseccfully update Trackers");
+ document.getElementById("dadd").style.display="none";
+  console.log(Trackers[editindex]);
+}
+
